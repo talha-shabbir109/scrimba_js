89232750366d4993642da4598e5ebf31894ff84a
Muhammad Talha <khokhartalha114@gmail.com>
2024-09-18 06:22:41 +0500

    first string Practice

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-let countEl = document.getElementById("count-el")
-let count = 0
+// Create two variables, name and greeting. The name variable should store your name,
+// and the greeting should store e.g. "Hi, my name is "
 
-function increment() {
-    count = count + 1
-    countEl.innerText = count
-}
+// Create a third variable, myGreeting, that contatenates the two strings
+// Log myGreeting to the console
 
-// 1. Create a function, save(), which logs out the count when it's called
-
-function save() {
-    console.log(count)
-}
+let name = "Talha"
+let greeting = "Hi, my name is "
+let myGreeting = greeting + name
+console.log(myGreeting)
