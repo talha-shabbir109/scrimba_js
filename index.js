eb595e3dcd15d1403f2cb3f4ee2f86a2920a8baf
Muhammad Talha <khokhartalha114@gmail.com>
2024-09-26 15:51:14 +0500

    Create a function

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0
+let name = "Linda"
+let greeting = "Hi there"
 
-function increment() {
-    count += 1
-    countEl.textContent = count
+// Create a function that logs out "Hi there, Linda!" when called
+
+function greetLinda() {
+    console.log(greeting + ", " + name + "!")
 }
 
-function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0
-    count = 0
-}
-
+greetLinda()
