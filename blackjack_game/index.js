63e9cbe00b568f2f8d56e74b8e37ec2b9f9925d1
Muhammad Talha <khokhartalha114@gmail.com>
2024-10-04 16:30:56 +0500

    add more button

--- blackjack_game/index.js
+++ blackjack_game/index.js
@@ -1,13 +1,12 @@
 let firstCard = 10
 let secondCard = 4
-// 1. Create a new array - cards - that contains firstCard and secondCard
 let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el") 
+let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
 function startGame() {
@@ -15,7 +14,6 @@ function startGame() {
 }
 
 function renderGame() {
-    // 2. Refer to the cards array when rendering out the cards
     cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
@@ -34,5 +32,8 @@ function renderGame() {
 function newCard() {
     let card = 6
     sum += card
+    // Push the card to the cards array
+    cards.push(card)
+    console.log(cards)
     renderGame()
 }
