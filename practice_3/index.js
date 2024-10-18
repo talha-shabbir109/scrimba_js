d7e7affbcb370a3013a9e115423dabae407d3969
Muhammad Talha <khokhartalha114@gmail.com>
2024-10-18 11:17:44 +0500

    Log out items in an array

--- practice_3/index.js
+++ practice_3/index.js
@@ -1,21 +1,13 @@
-// SETTING THE STAGE
-const player = "Per"
-const opponent = "Nick"
-const game = "AmazingFighter"
+let myCourses = ["Learn CSS Animations", "UI Design Fundamentals", "Intro to Clean Code"]
 
-let points = 0
-let hasWon = false
+// Create a function that takes a single parameter, an array,
+// and logs all the items of the array to the console.
+// Call the function while passing in myCourses as an argument
 
-// PLAYING THE GAME
-points += 100
-hasWon = true
-
-// ANNOUNCING THE WINNER
-if (hasWon) {
-    console.log(`${player} got ${points} points and won the ${game} game!`)
-} else {
-    console.log(`The winner is ${opponent}! ${player} lost the game`)
+function logItems(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i])
+    }
 }
 
-// Go through all variables and decide if they should be let or const
-// Change the console logs to use template strings instead of double quotes
+logItems(myCourses)
