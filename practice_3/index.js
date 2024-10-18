b02dce0f354b682986d3515eb284d023c1c2fe22
Muhammad Talha <khokhartalha114@gmail.com>
2024-10-18 11:19:43 +0500

    save to localStorage

--- practice_3/index.js
+++ practice_3/index.js
@@ -1,13 +1,6 @@
-let myCourses = ["Learn CSS Animations", "UI Design Fundamentals", "Intro to Clean Code"]
+// Save a value to localStorage
+// Delete your code and refresh the page
+// Fetch your value from localStorage and log it out
 
-// Create a function that takes a single parameter, an array,
-// and logs all the items of the array to the console.
-// Call the function while passing in myCourses as an argument
-
-function logItems(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        console.log(arr[i])
-    }
-}
-
-logItems(myCourses)
+let myCredits = localStorage.getItem("myCredits")
+console.log(myCredits)
